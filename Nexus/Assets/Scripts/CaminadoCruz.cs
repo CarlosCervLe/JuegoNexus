4433c431f0298adfd980a0f903db38a325c95dc3
Jack20192020 <142038930+Jack20192020@users.noreply.github.com>
2024-04-17 10:39:56 -0500

    Con el gitignore xd

---
+++ Nexus/Assets/Scripts/CaminadoCruz.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaminadoCruz : MonoBehaviour
+{
+    private Rigidbody cuerpo;
+    private Animator miAnimador;
+    public float velocidadMovi;
+
+    void Start()
+    {
+        cuerpo = GetComponent<Rigidbody>();
+        miAnimador = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float verti = Input.GetAxis("Vertical");
+
+        miAnimador.SetFloat("MOV_LATERAL", horiz);
+        miAnimador.SetFloat("MOV_FRONTAL", verti);
+
+        Vector3 direc = (transform.forward * verti + transform.right * horiz).normalized;
+        cuerpo.velocity = direc * velocidadMovi;
+
+    }
+}
